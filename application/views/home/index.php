09796d4a45addc5c91c4c54ae84c2a87200eab96
pavelpravchev <pavel.pravchev@gmail.com>
2014-10-16 16:49:47 +0300

    home page, not functional yet.

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -6,12 +6,12 @@
     $assetMgr = Yii::app()->assetPublisher;
 ?>
 
-<div class="row">
+<div class="row" align="center">
     <?php
         $organisation = \application\models\db\Organisation::model()->findByPk(3);
         $logoImg = $organisation->LogoImg;
-    ?>
-    <?php
+        $prizeImg = $organisation->PrizeImg;
+
         echo CHtml::image(
             // The asset publisher requires that a filepath, not a URL, is supplied. It will then take that file and put
             // it in a folder that's accessible to website visitors (it returns the URL of the newly created, web-accessible file).
@@ -20,7 +20,7 @@
             //
             // If the file is already inside the webroot (public_html), then you don't need to use the asset publisher
             // as it is already accessible to website visitors, just pass the URL directly to CHtml::image().
-            Yii::app()->assetPublisher->publish('file/path/to/image.png'),
+            Yii::app()->assetPublisher->publish(Yii::getPathOfAlias('application.views.Uploads.images') . '/'.$logoImg->url),
 
             // Alternative text, required for accessibility-valid HTML.
             'Alternative Text',
@@ -29,8 +29,38 @@
             array('class' => 'img-responsive')
         );
     ?>
-    <?php echo CHtml::image(Yii::app()->baseUrl.$assetMgr->embed($logoImg->url), 'alt', array('class'=> 'img-responsive')); ?>
+    <div class="col-md-offset-4 col-md-8">
+        <h1 style="font-weight: bold; text-decoration: underline;">RATE US TO WIN !</h1>
+    </div>
+    <div class="col-md-4">
+        <?php
+        echo CHtml::image(
+            Yii::app()->assetPublisher->publish(Yii::getPathOfAlias('application.views.Uploads.images') . '/'.$prizeImg->url),
+            'Alternative Text',
+            array('class' => 'img-responsive')
+        );
+        ?>
+    </div>
+    <div class="col-md-8">
+        <div class="progress">
+          <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+            <span class="sr-only">60% Complete</span>
+          </div>
+        </div>
+    </div>
+    <?php $surveys = $organisation->Surveys;?>
     <pre>
+        <?php var_dump($surveys); ?>
     </pre>
-
+<?php for ($i=0; $i<3; $i++):?>
+    <ul class="nav nav-tabs" role="tablist" style="display:none">
+      <li class="active">
+        <a href="#home" role="tab" data-toggle="tab">Home</a>
+    </li>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <div class="tab-pane active" id="home">A</div>
+    </div>
+<?php endfor; ?>
 </div>
+
