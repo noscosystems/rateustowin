d6559b8e7a320f19cbd2af99f3253d467ff722be
clive dann <clive.dann@googlemail.com>
2014-10-22 09:58:14 +0100

    ``

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -71,8 +71,8 @@
         </div>
     <?php endfor; ?>
     <div class="tab-pane fade" id="Credentials">
-        <h4>Thank you for completing this survey.</h4>
-        <h3>Please enter the following information so we can inform you if you win</h3>
+        <h3>Thank you for completing this survey.</h3>
+        <h4>Please enter the following information so we can inform you if you win</h4>
         <div class="row">
         <div class="col-sm-2 col-sm-offset-2 control-label">First Name:</div>
             <div class="col-sm-6">
