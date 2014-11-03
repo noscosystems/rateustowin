21ad37d04403804d8fac3e8d9fe1e6607488deff
pavelpravchev <pavel.pravchev@gmail.com>
2014-11-03 16:45:37 +0200

    small fix regarding query that takes all the branches related to an organisation

--- application/modules/admin/controllers/StatsController.php
+++ application/modules/admin/controllers/StatsController.php
@@ -20,7 +20,7 @@
                 $branch = Yii::app()->db->createCommand()
                             ->select('b.id, b.name')
                             ->from('branches b')
-                            ->where('b.id=:id', array(':id' => $orgSelect->model->id))
+                            ->where('b.organisationId=:id', array(':id' => $orgSelect->model->id))
                             ->queryAll();
 
                 foreach ($branch as $k => $v){
@@ -28,7 +28,7 @@
                 }
 
                 if ($enquiryForm->submitted() && $enquiryForm->validate()){
-                    // $sql = SELECT customer.firstName, survey.name, question.questTxt, answer.answerTxt FROM answer JOIN answersheet ON answer.ansSheetId = answersheet.id JOIN branches as br ON `answersheet`.`branchId` = `br`.`id` JOIN customer ON answersheet.customerId = customer.id JOIN organisation ON `br`.organisationId = organisation.id JOIN survey ON survey.orgId = organisation.id JOIN question ON survey.id = question.surveyId WHERE `br`.`id` = 2
+                    // $sql =   SELECT customer.firstName, survey.name, question.questTxt, answer.answerTxt FROM answer JOIN answersheet ON answer.ansSheetId = answersheet.id JOIN branches as br ON `answersheet`.`branchId` = `br`.`id` JOIN customer ON answersheet.customerId = customer.id JOIN organisation ON `br`.organisationId = organisation.id JOIN survey ON survey.orgId = organisation.id JOIN question ON survey.id = question.surveyId WHERE `br`.`id` = 2
                 }
 
                 // $form->elements['name']->items = array();
