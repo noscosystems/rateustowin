cc4f3c113ce374778d5c64907749bf8c0415aa9e
pavelpravchev <pavel.pravchev@gmail.com>
2014-11-06 10:33:02 +0200

    debugging

--- application/modules/admin/controllers/StatsController.php
+++ application/modules/admin/controllers/StatsController.php
@@ -90,8 +90,8 @@
 
                         if ($ind == 0 ){
                             //unset($row['id']); 
-                            $report_transp[]  = $row;
-                            $report_transp[]['Q'.$ind] = $answer;
+                            $report_transp[$ind]  = $row;
+                            $report_transp[$ind]['Q'.$ind] = $answer;
                         
                         }
                         elseif (isset($report_transp[$last])){
